7e99d95a5eeaa30e2fcb087b62c8a4efa7605094
EasternJamm <apetremarianioan@gmail.com>
2023-08-09 17:58:47 +0300

    4. Created sing up component, the form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,7 +3,19 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
+  /* useEffect(() => {
+    async function fetchData() {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    }
+    fetchData();
+  }, []); //will be called once if the 2nd parameter is empty
+ */
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -13,6 +25,7 @@ const SignIn = () => {
     <div>
       <h1>Sign in Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
