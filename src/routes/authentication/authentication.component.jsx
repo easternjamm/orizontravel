559bf543e4fc606024766519c49dcfc9994290eb
EasternJamm <apetremarianioan@gmail.com>
2023-08-10 17:14:33 +0300

    5. Created authentication page component alongside sign in form

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,11 +1,9 @@
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
-
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-const SignIn = () => {
+import "./authentication.styles.scss";
+
+const Authentication = () => {
   /* useEffect(() => {
     async function fetchData() {
       const response = await getRedirectResult(auth);
@@ -16,18 +14,18 @@ const SignIn = () => {
     fetchData();
   }, []); //will be called once if the 2nd parameter is empty
  */
-  const logGoogleUser = async () => {
+  /* const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-  };
+  }; */
 
   return (
-    <div>
-      <h1>Sign in Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    <div className='authentication-container'>
+      {/* <button onClick={logGoogleUser}>Sign in with Google Popup</button> */}
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
